5376f83c9c9b637b63c3db0f45644e2eccffbf2e
Byeong Min Cho <merrybmc@gmail.com>
2022-10-29 15:44:17 +0900

    JJin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,57 @@
-import HOME from "./sources/home.png";
-import styled from "styled-components";
-import { useState } from "react";
-import Login from "./Login";
+import HOME from './sources/home.png';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-// zzz
+  const navigate = useNavigate();
   return (
     <>
       <StWrap>
-aaa
         <StSignUpBox>
-          <StSignUpBtn>계정 만들기</StSignUpBtn>
+          <StSignUpBtn
+            onClick={() => {
+              navigate('/signup');
+            }}
+          >
+            계정 만들기
+          </StSignUpBtn>
         </StSignUpBox>
         <StCoverImg src={HOME} />
       </StWrap>
     </>
-
   );
 }
-//df
-const BackgroundImg = styled.image`
-  display: flex;
-  position: absolute;
+
+const StWrap = styled.div`
+  position: relative;
 `;
 
-const LoginButton = styled.button`
-  width: 120px;
-  height: 46px;
-  border: none;
-  border-radius: 30px;
-  background-color: transparent;
-  position: relative;
-  bottom: 955px;
-  margin-left: 1726px;
-  color: black;
-  font-size: x-large;
-  font-family: sans-serif;
+const StCoverImg = styled.img`
+  height: 100%;
+  vertical-align: middle;
+`;
 
+const StSignUpBox = styled.div`
+  height: auto;
+  margin-top: 628px;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const StSignUpBtn = styled.button`
+  width: 267px;
+  height: 44px;
+  position: absolute;
+  background-color: #fd5547;
+  font-size: 20px;
+  color: white;
+  font-weight: bold;
+  border: 1px solid #fd5547;
+  border-radius: 25px;
+  cursor: pointer;
+  &:hover {
+    background-color: #d74f3b;
+    border: 1px solid #d74f3b;
+  }
 `;
