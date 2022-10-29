e009a8de1be753f2491bfa5ce0d305ee89a70e99
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2022-10-29 13:47:40 +0900

    login 첫번째

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,56 @@
-import styled from 'styled-components';
-import HOME from './sources/home.png';
-import { useNavigate } from 'react-router-dom';
+import HOME from "./sources/home.png";
+import styled from "styled-components";
+import { useState } from "react";
+import Login from "./Login";
 
 export default function Home() {
-  const navigate = useNavigate();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const onOpenModal = () => {
+    setModalOpen(true);
+  };
+  const onCloseModal = () => {
+    setModalOpen(false);
+  };
   return (
-    <>
-      <StWrap>
-        <StSignUpBox>
-          <StSignUpBtn onClick={() => navigate('/signup')}>계정 만들기</StSignUpBtn>
-        </StSignUpBox>
-        <StCoverImg src={HOME} />
-      </StWrap>
-    </>
+    <div id="BackgroundImg">
+      <img src={HOME} />
+      <LoginButton
+        type="button"
+        onClick={() => {
+          setModalOpen(!modalOpen);
+        }}
+      >
+        로그인
+        {modalOpen && (
+          <Login
+            visible={onOpenModal}
+            closable={true}
+            maskClosable={true}
+            onClose={onCloseModal}
+          ></Login>
+        )}
+      </LoginButton>
+    </div>
   );
 }
 
-const StWrap = styled.div`
-  position: relative;
-`;
-
-const StCoverImg = styled.img`
-  height: 100%;
-  vertical-align: middle;
-`;
-
-const StSignUpBox = styled.div`
-  width: 100%;
-  height: auto;
-  margin-top: 628px;
-  position: absolute;
+const BackgroundImg = styled.image`
   display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const StSignUpBtn = styled.button`
-  width: 267px;
-  height: 44px;
   position: absolute;
-  background-color: #fd5547;
-  font-size: 20px;
-  color: white;
-  margin-right: 40px;
-  font-weight: bold;
-  border: 1px solid #fd5547;
-  border-radius: 25px;
-  cursor: pointer;
-  &:hover {
-    background-color: #d74f3b;
-    border: 1px solid #d74f3b;
-  }
+`;
+
+const LoginButton = styled.button`
+  width: 120px;
+  height: 46px;
+  border: none;
+  border-radius: 30px;
+  background-color: transparent;
+  position: relative;
+  bottom: 955px;
+  margin-left: 1726px;
+  color: black;
+  font-size: x-large;
+  font-family: sans-serif;
+
 `;
